96f36d2b1964ffca40d0e13ce04af0ae4e015450
anggiassafa <anggipermataassafa@gmail.com>
2023-10-10 08:47:26 +0700

    contoh1

---
+++ contoh1.php
@@ -0,0 +1,11 @@
+<?php 
+class Contoh1 extends CI_Controller
+{ 
+    public function index()
+    { 
+        echo "<h1>Perkenalkan</h1>"; 
+        echo "Nama saya Anggi Permata Assafa
+            Saya tinggal di Jakarta
+            Hobi saya membaca buku";
+    } 
+}
